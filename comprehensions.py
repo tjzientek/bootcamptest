db5e3a427b1cc79ef4dc5a2acdfca7fe7f68eac9
T.J. Zientek <37338907+tjzientek@users.noreply.github.com>
2018-05-19 13:55:35 -0400

    Initial work on my repo

---
+++ comprehensions.py
@@ -0,0 +1,15 @@
+names = []
+for _ in range(5):
+    name = input("Please enter the name of someone you know. ")
+    names.append(name)
+
+lowercased = [name.lower() for name in names]
+
+titlecased = [name.title() for name in lowercased]
+
+invitations = [
+    f"Dear {name}, please come to the wedding this Staturday!" for name in titlecased]
+
+for invitation in invitations:
+    print(invitation)
+
